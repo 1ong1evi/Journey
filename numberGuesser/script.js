e3e41b0894e9a84c0ddc3010616bb7ee2cdcd23a
1ong1evi <86071717+1ong1evi@users.noreply.github.com>
2021-12-03 08:31:57 -0600

    Number Guesser
    Nothing more than a number guesser

---
+++ numberGuesser/script.js
@@ -0,0 +1,43 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 9)
+};
+
+const compareGuesses = (userGuess, computerGuess, targetNumber) => {
+    //most likely can clean this code up more...
+    if (Math.abs(targetNumber - computerGuess) === Math.abs(targetNumber - userGuess)) {
+        return true;
+    } else if (Math.abs(targetNumber - computerGuess) < Math.abs(targetNumber - userGuess)) {
+        return false;
+    } else if (Math.abs(targetNumber - computerGuess) > Math.abs(targetNumber - userGuess)) {
+        return true;
+    }
+
+};
+
+const updateScore = str => {
+    //Might have to remove the return will check back letter
+    switch (str) {
+        case 'human':
+            return humanScore += 1;
+            break;
+        case 'computer':
+            return computerScore += 1;
+            break;
+        default:
+            return 'error';
+    }
+};
+
+const advanceRound = currentRoundNumber => {
+    return currentRoundNumber += 1;
+}
+
+updateScore('human');
+console.log(humanScore); // To confirm that this value increased by 1
+updateScore('computer');
+console.log(computerScore); // To confirm that this value increased by 1
