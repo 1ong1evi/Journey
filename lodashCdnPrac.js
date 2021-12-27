07750db8afaf9c099933f47c0613ec66fa5ba49e
Levi <86071717+1ong1evi@users.noreply.github.com>
2021-12-27 07:03:36 -0600

    cleaning up tests on the functions

--- lodashCdnPrac.js
+++ lodashCdnPrac.js
@@ -1,8 +1,8 @@
 const _ = {
-    otherClamp(number, lower, upper) { //Ideate
+    otherClamp(number, lower, upper) { 
        return Math.max(lower, Math.min(number, upper));
     },
-    testClamp(number, lower, upper) { //Specify
+    testClamp(number, lower, upper) { 
         if(number < lower) {
             return lower;
         } else if (number > upper) {
@@ -11,25 +11,16 @@ const _ = {
             return number;
         }
     },
-    clamp(number, lower, upper) { //Implement
+    clamp(number, lower, upper) {
         lowerClampedValue = Math.max(number, lower);
         clampedValue =  Math.min(lowerClampedValue, upper); 
     return clampedValue;
     },
-    //Specify,Ideate,Implement
     inRange(number, start, end) {
-        //console.log(number);
-        //console.log(start);
-        //console.log(end); testing area
-        //console.log('------');
         if(end === undefined) {
             end = start;
             start = 0;
         }
-        //console.log(number);
-        //console.log(start);
-        //console.log(end); testing area
-        //console.log('------');
          if (start > end) {
             temp = end;
             end = start;
@@ -39,7 +30,6 @@ const _ = {
 
     return isInRange;
     },
-    //Specify,Ideate,Implement
     words(s){
         a = s.split(' ');
         return a;
@@ -104,18 +94,3 @@ const _ = {
         return arrayChunks;
     }
 }
-//testing area much testing much learning
-
-/*
-    Specify the functionality of the method we are implementing
-    Ideate a game plan for how to implement this functionality in code
-    Implement our game plan
-    Test our code to ensure it works as expected
-*/
-//console.log(_.otherClamp(7, 4, 6));
-//console.log(_.testClamp(7, 4, 6));
-//console.log(_.clamp(7, 4, 6));
-//console.log(_.inRange(3, 7));
-//console.log(_.words('How are you?'));
-//console.log(_.pad('Really', 16));
-//decide to test against the infile tests doing my own flowcharts/specification 
