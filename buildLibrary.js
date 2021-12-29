f67b9fb8d5976eccc6ae23dc6a5b91cb48954ad6
Levi <longlevi6@gmail.com>
2021-12-29 14:31:05 -0600

    Built a library with a parent class Media and two subclasses Movie & Book

--- buildLibrary.js
+++ buildLibrary.js
@@ -27,3 +27,60 @@ class Media {
        this.ratings.push(value);
    }
 }
+
+class Book extends Media {
+    constructor(author, title, pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
+    }
+    get author() {
+        return this._author;
+    }
+    get pages() {
+        return this._pages;
+    }
+}
+
+class Movie extends Media {
+    constructor(director, title, runTime) {
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+    }
+    get director() {
+        return this._director;
+    }
+    get runTime() {
+        return this._runTime;
+    }
+}
+
+//creates a book instance
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+console.log(`Book: ${historyOfEverything.title}`);
+historyOfEverything.toggleCheckOutStatus();
+
+//checks if checked out 
+console.log(`Is the book checked out? ${historyOfEverything.isCheckedOut}`);
+
+//adds three ratings to see if get average rating works
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(`The average rating is ${historyOfEverything.getAverageRating()}`);
+
+
+//creats a movie instance
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+console.log(`Movie: ${speed.title}`);
+speed.toggleCheckOutStatus();
+
+//checks if checked out 
+console.log(`Is the movie checked out? ${speed.isCheckedOut}`);
+
+//adds three ratings to see if get average rating works
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+console.log(`The average rating is ${speed.getAverageRating()}`);
