8fbeb938d3dfc426776acd0f63c632cc60118297
Levi <longlevi6@gmail.com>
2021-12-29 09:25:31 -0600

    parent class for future subclasses

--- buildLibrary.js
+++ buildLibrary.js
@@ -1,1 +1,29 @@
-//YO
+class Media {
+    constructor(title) {
+        this._title = title;
+        this._isCheckedOut = false;
+        this._ratings = []; 
+    }
+   get title() {
+       return this._title;
+   }
+   get isCheckedOut() {
+       return this._isCheckedOut;
+   }
+   get ratings() {
+       return this._ratings;
+   }
+   set isCheckedOut(value) {
+    this._isCheckedOut = value;
+   }
+   toggleCheckOutStatus() {
+       this._isCheckedOut = !this.isCheckedOut
+   }
+   getAverageRating() {
+       let ratingsSum = this.ratings.reduce((accumulator, ratings) => accumulator + ratings);
+       return ratingsSum / this.ratings.length;
+   }
+   addRating(value) {
+       this.ratings.push(value);
+   }
+}
