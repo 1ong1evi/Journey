a119a87a0ea7708a5455a44f2168aa7c36e37ab9
Levi <longlevi6@gmail.com>
2021-12-30 09:39:26 -0600

    finished school catalogue of primary, high, middle

--- schoolCatalogue.js
+++ schoolCatalogue.js
@@ -1,4 +1,4 @@
-class school {
+class School {
     constructor(name, level, numberOfStudents) {
       this._name = name;
       this._level = level;
@@ -28,4 +28,37 @@ class school {
         return substituteTeachers[rand];
     }
 }
-  
+class PrimarySchool extends School {
+  constructor(name, numberOfStudents, pickupPolicy) {
+    super(name, 'primary', numberOfStudents);
+    this._pickupPolicy = pickupPolicy;
+  }
+  get pickupPolicy() {
+    return this._pickupPolicy;
+  }
+}
+  class HighSchool extends School {
+    constructor(name, numberOfStudents, sportsTeams) {
+      super(name, 'high', numberOfStudents);
+      this._sportsTeams = sportsTeams;
+    }
+    get sportsTeams() {
+      return this._sportsTeams;
+    }
+  }
+  class MiddleSchool extends School {
+    constructor(name, numberOfStudents) {
+      super(name, 'middle', numberOfStudents);
+    }
+  }
+//PrimarySchool instance
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+lorraineHansbury.quickFacts();
+//Gives Lorraine Hansbury a random sub for the day
+const sub = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+//HishSchool instance
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+alSmith.quickFacts();
+//MiddleSchool instance
+const cedarMiddle = new MiddleSchool('Cedar Middle', 406)
+cedarMiddle.quickFacts();
