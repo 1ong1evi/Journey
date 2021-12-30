395233960ee3da6db4cf4c9f07507a81a8a0b339
Levi <longlevi6@gmail.com>
2021-12-30 08:55:30 -0600

    first commit of School Catalogues parent class

---
+++ schoolCatalogue.js
@@ -0,0 +1,31 @@
+class school {
+    constructor(name, level, numberOfStudents) {
+      this._name = name;
+      this._level = level;
+      this._numberOfStudents = numberOfStudents;
+    }
+    get name() {
+      return this._name;
+    }
+    get level() {
+      return this._level;
+    }
+    get numberOfStudents() {
+      return this._numberOfStudents;
+    }
+    set numberOfStudents(value) {
+      if(typeof numberOfStudents === "number") {
+        this._numberOfStudents = value;
+      } else {
+        console.log('Invalid input: numberOfStudents must be set to a Number.');
+      }
+    }
+    quickFacts() {
+        console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`);
+    }
+    static pickSubstituteTeacher(substituteTeachers) {
+        const rand = Math.floor(Math.random() * substituteTeachers.length);
+        return substituteTeachers[rand];
+    }
+}
+  
