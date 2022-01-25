f1107c48f1b99ea1fa64d6dabddbeeb83f42b15f
Levi <longlevi6@gmail.com>
2022-01-25 12:35:26 -0600

    comments made

--- fizzBuzz.c
+++ fizzBuzz.c
@@ -6,7 +6,8 @@ int main()
 
     for (i = 1; i <= 100; i++)
     {
-        // checks for 15 before other iterations
+        // checks for 15 before other iterations HAVE to check this case before any other or it wont work
+        // the only problem among this interview question
         if (i % 3 == 0 && i % 5 == 0)
         {
             printf("FizzBuzz\n");
