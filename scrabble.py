960c7f9d52594b97b82e1a02bc6d638b159dfe48
Levi <longlevi6@gmail.com>
2022-03-15 09:20:51 -0500

    letters and points to be made into a dictionary

--- scrabble.py
+++ scrabble.py
@@ -1,3 +1,4 @@
+# letters and points to be made into a dictionary
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
            "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1,
