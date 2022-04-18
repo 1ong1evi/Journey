995e816158858b35b6dc558197ea7b19a6bde3a8
Levi <longlevi6@gmail.com>
2022-04-18 09:01:22 -0500

    method to return the values of the linked list

--- linkedList.py
+++ linkedList.py
@@ -24,3 +24,12 @@ class LinkedList:
         new_node = Node(new_value)
         new_node.set_next_node(self.head_node)
         self.head_node = new_node
+
+    def stringfy_list(self):
+        stringfy = ""
+        current_node = self.get_head_node()
+        while current_node:
+            if current_node.get_value() != None:
+                stringfy += str(current_node.get_value()) + "\n"
+                current_node = current_node.get_next_node()
+        return stringfy
