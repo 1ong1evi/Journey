c89242b036c6a864ffcb73a73b6293887dba0c3c
Levi <longlevi6@gmail.com>
2022-04-22 09:01:13 -0500

    finished the nth last node finder

--- linkedList.py
+++ linkedList.py
@@ -88,5 +88,16 @@ class LinkedList:
         node1.set_next_node(node2.get_next_node())
         node2.set_next_node(temp)
 
-    def get_nth_node(self):
-        pass
+    def get_nth_node(self, n):
+        current = None
+        tail_seeker = self.head_node
+        count = 1
+        while tail_seeker:
+            tail_seeker = tail_seeker.get_next_node()
+            count += 1
+            if count >= n + 1:
+                if current is None:
+                    current = self.get_head_node()
+                else:
+                    current = current.get_next_node()
+        return current.get_value()
