e5b3131679c78d6c60951017c5147e1d6f5dd1e9
Levi Long <longlevi6@gmail.com>
2022-06-03 10:03:27 -0500

    initialized the node method

--- queue.py
+++ queue.py
@@ -1,1 +1,5 @@
-# first comment
+class Node:
+    def __init__(self, value, next_node=None, prev_node=None):
+        self.value = value
+        self.next_node = next_node
+        self.prev_node = prev_node 
