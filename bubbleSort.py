7373b423eb369a60c52b4b98f8d0f31a93de5b8e
Levi Long <longlevi6@gmail.com>
2022-08-18 12:51:39 -0500

    bubble sort start

--- bubbleSort.py
+++ bubbleSort.py
@@ -2,3 +2,9 @@ def swap(arr, index_1, index_2):
     temp = arr[index_1]
     arr[index_1] = arr[index_2]
     arr[index_2] = temp
+
+def bubble_sort(arr):
+    iteration_count = 0
+    for i in range(len(arr)):
+        # iterate through unplaced elements
+        pass
